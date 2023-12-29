608d28d6fd40d2138e6e1233e3a230e726048e80
Admin <karapetyanstepa@gmail.com>
2023-12-29 20:54:55 +0300

    final version commit

--- src/by/it_academy/homeworks/HW1.java
+++ src/by/it_academy/homeworks/HW1.java
@@ -7,31 +7,26 @@ public class HW1 {
 
         Scanner sc = new Scanner(System.in);
         double averageScore = 0;
-        boolean isError = false;
 
         System.out.print("Enter the number of subjects: ");
         int numbersOfSubjects = sc.nextInt();
 
-        if (numbersOfSubjects > 0) {
-            for (int i = 1; i <= numbersOfSubjects; i++) {
-                System.out.print("Enter a score of subject №" + i + ": ");
-                double currentScore = sc.nextDouble();
-
-                if ((currentScore >= 1) && (currentScore <= 10) && (currentScore % 1 == 0)) {
-                    averageScore += currentScore;
-                } else {
-                    System.out.println("Error! Enter an integer from 1 to 10.");
-                    isError = true;
-                    break;
-                }
-            }
-
-            if (isError == false) {
-                averageScore /= numbersOfSubjects;
-                System.out.println("Average score: " + averageScore);
-            }
-        } else {
+        if (numbersOfSubjects <= 0) {
             System.out.println("Enter the correct value!");
+            return;
         }
+        for (int i = 1; i <= numbersOfSubjects; i++) {
+            System.out.print("Enter a score of subject №" + i + ": ");
+            double currentScore = sc.nextDouble();
+
+            if ((currentScore >= 1) && (currentScore <= 10) && (currentScore % 1 == 0)) {
+                averageScore += currentScore;
+            } else {
+                System.out.println("Error! Enter an integer from 1 to 10.");
+                return;
+            }
+        }
+        averageScore /= numbersOfSubjects;
+        System.out.println("Average score: " + averageScore);
     }
 }
