955408c58c18387de671069a80781d48ad6eb7cd
Степа Карапетян <karapetyanstepa@gmail.com>
2024-01-11 14:16:02 +0300

    homework2 commit

---
+++ src/by/it_academy/homeworks/hw2/Task1.java
@@ -0,0 +1,37 @@
+package by.it_academy.homeworks.hw2;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        double xCircle = scannerCoordinate("x-coordinate", "circle");
+        double yCircle = scannerCoordinate("y-coordinate", "circle");
+        double radius = scannerCoordinate("radius", "circle");
+        double xPoint = scannerCoordinate("x-coordinate", "point");
+        double yPoint = scannerCoordinate("y-coordinate", "point");
+        printIsOutsidePoint(xCircle, yCircle, radius, xPoint, yPoint);
+    }
+
+    public static double scannerCoordinate(String nameCoordinate, String ownerCoordinate) {
+        Scanner sc = new Scanner(System.in);
+        sc.useLocale(Locale.US);
+        System.out.println("Enter the " + nameCoordinate + " of the " + ownerCoordinate + ":");
+        double coordinate = sc.nextDouble();
+        if (nameCoordinate.equals("radius") && (coordinate < 0)) {
+            System.out.println("Enter a valid value!");
+            return scannerCoordinate(nameCoordinate, ownerCoordinate);
+        }
+        else
+            return coordinate;
+    }
+
+    public static void printIsOutsidePoint(double xCircle, double yCircle, double radius, double xPoint, double yPoint) {
+        double distance = Math.sqrt(Math.pow(xPoint - xCircle, 2) + Math.pow(yPoint - yCircle, 2));
+        if (distance > radius) {
+            System.out.println("The point is outside the circle.");
+        } else {
+            System.out.println("The point is inside or on the circle.");
+        }
+    }
+}
---
+++ src/by/it_academy/homeworks/hw2/Task3.java
@@ -0,0 +1,27 @@
+package by.it_academy.homeworks.hw2;
+
+import java.util.Random;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int seconds = getRandomNumberSeconds();
+        System.out.println("The are " + seconds + " seconds left until the end.");
+        printTimeUntilTheEnd(seconds);
+    }
+
+    public static int getRandomNumberSeconds(){
+        Random random = new Random();
+       return random.nextInt(28800);
+    }
+
+    public static void printTimeUntilTheEnd(int seconds){
+        int hours = seconds / 3600;
+        String timeToEnd = switch (hours){
+            case 2,3,4,5,6,7,8 -> "There are " + hours + " hours left until the end.";
+            case 1 -> "There is " + hours + " hour left until the end.";
+            case 0 -> "Less than an hour left until the end.";
+            default -> "There's still a little bit left until the end.";
+        };
+        System.out.println(timeToEnd);
+    }
+}
