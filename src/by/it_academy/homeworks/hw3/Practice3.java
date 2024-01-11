7cc18f35808e0e7faa11e4deaf4f5c22720ed99a
Степа Карапетян <karapetyanstepa@gmail.com>
2024-01-11 19:50:47 +0300

    lastTasksDoing

--- src/by/it_academy/homeworks/hw3/Practice3.java
+++ src/by/it_academy/homeworks/hw3/Practice3.java
@@ -6,7 +6,21 @@ public class Practice3 {
     public static void main(String[] args) {
         //printSumAndMultiplicat(scannerInt());
         //printTheBiggerNumberAndIndex(scannerInt());
-        printCountMistakes();
+        //printCountMistakes();
+
+
+//        int count = 0;
+//        for (int hours = 0; hours < 24; hours++) {
+//            for (int minutes = 0; minutes < 60; minutes++) {
+//                if (isSymmetrical(hours, minutes))
+//                    count++;
+//            }
+//        }
+//        System.out.println(count);
+
+        System.out.println(0);
+        System.out.println(1);
+        printNumersFibonachi();
     }
 
     public static int scannerInt() {
@@ -71,14 +85,43 @@ public class Practice3 {
         System.out.println("Max digit have position " + indexMax);
     }
 
-    public static void printCountMistakes(){
-            int unluckyNumbers = 0;
-            for (int i = 1; i <= 50000; i++) {
-                String strI = String.valueOf(i);
-                if (strI.contains("2")) {
-                    unluckyNumbers++;
-                }
+    public static void printCountMistakes() {
+        int unluckyNumbers = 0;
+        for (int i = 1; i <= 50000; i++) {
+            String strI = String.valueOf(i);
+            if (strI.contains("2")) {
+                unluckyNumbers++;
             }
+        }
         System.out.println("Mistake boxes are " + unluckyNumbers);
+    }
+
+    private static boolean isSymmetrical(int hours, int minutes) {
+        String hoursStr = String.valueOf(hours);
+        String minutesStr = String.valueOf(minutes);
+
+        if (hours < 10) {
+            hoursStr = "0" + hoursStr;
+        }
+
+        if (minutes < 10) {
+            minutesStr = "0" + minutesStr;
+        }
+
+        StringBuilder minutesStrSb = new StringBuilder(minutesStr);
+        String minutesReversStr = minutesStrSb.reverse().toString();
+
+        return hoursStr.equals(minutesReversStr);
+    }
+
+    public static void printNumersFibonachi() {
+        int firstNumber = 0;
+        int secondNumber = 1;
+        for (int i = 2; i < 10; i++) {
+            int nextNumber = firstNumber + secondNumber;
+            System.out.println(nextNumber);
+            firstNumber = secondNumber;
+            secondNumber = nextNumber;
+        }
     }
 }
