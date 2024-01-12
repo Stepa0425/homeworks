3bf089a30ef5dc2f2c40b2bc5ed48a23c1afae56
Степа Карапетян <karapetyanstepa@gmail.com>
2024-01-12 23:24:36 +0300

    homework 4 demo

---
+++ src/by/it_academy/homeworks/hw4/Task2.java
@@ -0,0 +1,20 @@
+package by.it_academy.homeworks.hw4;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] array = {5, 12, 9, -25, 8, 15, 7, 14, 20, -18, 9, 5};
+    }
+
+    public static int[] removeIdenticalElements(int[] array) {
+        int[] newArray;
+        int countRepeatEl = 0;
+        /*for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] == array[j]) {
+
+                }
+            }
+        }*/
+        return array;
+    }
+}
