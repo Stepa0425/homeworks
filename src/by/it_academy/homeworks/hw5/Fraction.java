e6b1b8d3fa46add83e86226d3b0c13822b5758cd
Stepa <karapetyanstepa@gmail.com>
2024-01-18 16:28:18 +0300

    demo Fractoinn

--- src/by/it_academy/homeworks/hw5/Fraction.java
+++ src/by/it_academy/homeworks/hw5/Fraction.java
@@ -32,6 +32,27 @@ public class Fraction {
         return this.numerator + " / " + this.denominator;
     }
 
+    private void shortenFraction() {
+        int resultNumerator = this.numerator / this.getLCD(this.numerator, this.denominator);
+        int resultDenominator = this.denominator / this.getLCD(this.numerator, this.denominator);
+        this.numerator = resultNumerator;
+        this.denominator = resultDenominator;
+    }
+
+    // LCD - Largest Common Divisor
+    private int getLCD(int number1, int number2) {
+        int divisor = Math.max(number1, number2);
+        int divisible = Math.min(number1, number2);
+        int remainder = divisor % divisible;
+
+        while (remainder != 0) {
+            divisor = divisible;
+            divisible = remainder;
+            remainder = divisor % divisible;
+        }
+
+        return divisible;
+    }
     /*Создайте метод класса, который будет выводить дробь в текстовую строку в формате x / y;
     метод, добавляющий (сложение) к текущей дроби дробь, переданную ему в параметре и возвращающий дробь - результат сложения;
     метод, умножающий (произведение) текущую дробь на число типа double, переданное ему в параметре и возвращающий дробь - результат умножения;
@@ -40,17 +61,25 @@ public class Fraction {
     public Fraction add(Fraction fraction1) {
         int commonDenominator = this.denominator * fraction1.denominator;
         int resultNominator = this.numerator * fraction1.denominator + this.denominator * fraction1.numerator;
-        return new Fraction(resultNominator, commonDenominator);
+        Fraction resultFraction = new Fraction(resultNominator, commonDenominator);
+        resultFraction.shortenFraction();
+        return resultFraction;
     }
 
-    public Fraction multiplicateDoubleNumber(double number) {
+    public Fraction multiplyDoubleNumber(double number) {
         double numerator = this.numerator * number;
-        int remainderOfDivision = (int)((numerator % 1) * 100000);
+        int remainderOfDivision = (int) ((numerator % 1) * 100000);
         numerator -= (remainderOfDivision / 100000);
 
-        Fraction fraction1 = new Fraction((int)numerator, this.denominator);
+        Fraction fraction1 = new Fraction((int) numerator, this.denominator);
         Fraction fraction2 = new Fraction(remainderOfDivision, this.denominator * 100000);
 
         return fraction1.add(fraction2);
     }
+
+    public Fraction divideByDoubleNumber(double number) {
+        return this.multiplyDoubleNumber(1 / number);
+    }
+
+
 }
--- src/by/it_academy/homeworks/hw5/Main.java
+++ src/by/it_academy/homeworks/hw5/Main.java
@@ -8,9 +8,9 @@ public class Main {
 //        System.out.println(fraction.getFractionString());
 //        Fraction resultFraction = fraction.add(new Fraction(3,6));
 //        System.out.println(resultFraction.getFractionString());
+        Fraction fraction0 = new Fraction(6, 3);
+        fraction0 = fraction0.multiplyDoubleNumber(2.2);
+        System.out.println(fraction0.getFractionString());
 
-        Fraction fraction1 = new Fraction(4, 5);
-        Fraction fraction2 = fraction1.multiplicateDoubleNumber(3.67);
-        System.out.println(fraction2.getFractionString());
     }
 }
