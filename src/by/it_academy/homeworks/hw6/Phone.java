17a95c9e36c91eece189ff36ae917972876ddc72
Stepa <karapetyanstepa@gmail.com>
2024-01-20 16:22:18 +0300

    final homework 6

--- src/by/it_academy/homeworks/hw6/Phone.java
+++ src/by/it_academy/homeworks/hw6/Phone.java
@@ -5,35 +5,35 @@ public class Phone {
     private String model;
     private int weight;
 
-    public Phone(){}
-    public Phone(String number, String model, int weight){
+
+    public Phone(String number, String model, int weight) {
         this.model = model;
         this.number = number;
         this.weight = weight;
     }
-    public String getNumber(){
+
+    public String getNumber() {
         return this.number;
     }
+
     public void receiveCall(String name) {
-        System.out.println("Звонит " + name);
-        System.out.println();
+        System.out.println("Звонит " + name + "\n");
     }
+
     public void receiveCall(String name, String number) {
-        System.out.println("Звонит " + name + " по номеру телефона " + number);
-        System.out.println();
+        System.out.println("Звонит " + name + " по номеру телефона " + number + "\n");
     }
-    public void sendMessage(String...numbers){
-        int length = numbers.length;
-        for(String number : numbers){
+
+    public void sendMessage(String... numbers) {
+        for (String number : numbers) {
             System.out.println(number);
         }
         System.out.println();
     }
-    public void printInfo(){
-        System.out.println("Phone model: " + this.model);
-        System.out.println("Phone weight: " + this.weight);
-        System.out.println("Phone number: " + this.number);
-        System.out.println();
+
+    @Override
+    public String toString() {
+        return "Phone:\nmodel - " + this.model + "\nweight - " + this.weight + "\nnumber - " + this.number + "\n";
     }
 
 }
--- src/by/it_academy/homeworks/hw6/Practice6.java
+++ src/by/it_academy/homeworks/hw6/Practice6.java
@@ -7,13 +7,15 @@ public class Practice6 {
         student.incrementEducationYear();
         System.out.println(student.getEducationYear());
 
+        //Phone p4 = new Phone();
+
         Phone p1 = new Phone("375445234978", "Samsung Galaxy S23", 140);
         Phone p2 = new Phone("375338579213", "Xiaomi Redmi 12 Plus", 167);
         Phone p3 = new Phone("375295478136", "Honor X9b", 156);
 
-        p1.printInfo();
-        p2.printInfo();
-        p3.printInfo();
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
 
         p1.receiveCall("Stepan");
         System.out.println(p1.getNumber());
@@ -27,9 +29,9 @@ public class Practice6 {
         System.out.println(p3.getNumber());
         System.out.println();
 
-        p1.receiveCall("Alex","375336512078");
-        p2.receiveCall("Vlad","375296896088");
-        p3.receiveCall("Andrew","375448716021");
+        p1.receiveCall("Alex", "375336512078");
+        p2.receiveCall("Vlad", "375296896088");
+        p3.receiveCall("Andrew", "375448716021");
 
         p1.sendMessage("375336512078", "375296896088", "375448716021");
         System.out.println();
