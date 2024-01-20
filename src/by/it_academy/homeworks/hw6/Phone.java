418d3c81f338d2e901d7e8ae7a1c06ee0f5fbb2b
Stepa <karapetyanstepa@gmail.com>
2024-01-20 13:52:01 +0300

    demo homework 6

--- src/by/it_academy/homeworks/hw6/Phone.java
+++ src/by/it_academy/homeworks/hw6/Phone.java
@@ -14,13 +14,26 @@ public class Phone {
     public String getNumber(){
         return this.number;
     }
-
     public void receiveCall(String name) {
         System.out.println("Звонит " + name);
+        System.out.println();
     }
-
     public void receiveCall(String name, String number) {
         System.out.println("Звонит " + name + " по номеру телефона " + number);
+        System.out.println();
+    }
+    public void sendMessage(String...numbers){
+        int length = numbers.length;
+        for(String number : numbers){
+            System.out.println(number);
+        }
+        System.out.println();
+    }
+    public void printInfo(){
+        System.out.println("Phone model: " + this.model);
+        System.out.println("Phone weight: " + this.weight);
+        System.out.println("Phone number: " + this.number);
+        System.out.println();
     }
 
 }
