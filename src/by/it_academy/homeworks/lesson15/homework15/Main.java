8eecb0194aefd83afea69e1065d1d7b8b8e30f17
Stepa <karapetyanstepa@gmail.com>
2024-02-27 16:27:48 +0300

    demo commit

--- src/by/it_academy/homeworks/lesson15/homework15/Main.java
+++ src/by/it_academy/homeworks/lesson15/homework15/Main.java
@@ -1,20 +1,9 @@
 package by.it_academy.homeworks.lesson15.homework15;
 
-import java.net.InterfaceAddress;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-/*
-● Удалить все содержащие “3”
-● Отсортировать сперва по числу (по возрастанию), потом по
-букве (по убыванию)
-● Отбросить первый и последний элемент
-● Привести в uppercase
-● Выдать на печать результат
-● Напечатать количество оставшихся элементов*/
 public class Main {
     public static void main(String[] args) {
         List<String> myList = Arrays.asList("a1", "a2", "a3", "b1", "b3", "c2", "c1", "c5");
@@ -27,17 +16,32 @@ public class Main {
         /*● Отсортировать сперва по числу (по возрастанию), потом по
             букве (по убыванию)*/
         List<String> sortedList = listWithout3.stream()
-                /*.sorted((e1,e2)->e2.substring(0).compareTo(e1.substring(0)))
+                /**Если выполнить в таком порядке, то будет отображение цифр в порядке увеличения,
+                 *  а при каждой цифре буквы в порядке уменьшения
+                 .sorted((e1,e2)->e2.substring(0).compareTo(e1.substring(0)))
+                 .sorted(Comparator.comparingInt(el -> Integer.valueOf(el.substring(1))))
+                 */
                 .sorted(Comparator.comparingInt(el -> Integer.valueOf(el.substring(1))))
-                */
-                .sorted(Comparator.comparingInt(el -> Integer.valueOf(el.substring(1))))
-                .sorted((e1,e2)->e2.substring(0).compareTo(e1.substring(0)))
+                .sorted((e1, e2) -> e2.substring(0).compareTo(e1.substring(0)))
                 .toList();
-        System.out.println(sortedList);
 
+        //● Отбросить первый и последний элемент
+        List<String> listWithoutFisrtLastEl = sortedList.stream()
+                .limit(sortedList.size() - 1)
+                .skip(1)
+                .toList();
 
-        //.sorted((el1, el2) -> el1.substring(0).compareTo(el2.substring(0)))
+        //● Привести в uppercase
+        List<String> upperCaseList = listWithoutFisrtLastEl.stream()
+                .map(el -> el.toUpperCase())
+                .toList();
 
+        //● Выдать на печать результат
+        upperCaseList.stream()
+                .forEach(el -> System.out.print(el + " "));
+
+        //● Напечатать количество оставшихся элементов
+        System.out.println("\nQuantity of elements = " + upperCaseList.size());
 
     }
 }
