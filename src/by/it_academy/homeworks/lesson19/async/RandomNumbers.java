9fdc4139b07ec24ab3c4ffc50f1100299659184b
Stepa0425 <karapetyanstepa@gmail.com>
2024-03-15 16:53:46 +0300

    end of lesson 19

--- src/by/it_academy/homeworks/lesson19/async/RandomNumbers.java
+++ src/by/it_academy/homeworks/lesson19/async/RandomNumbers.java
@@ -1,24 +1,33 @@
 package by.it_academy.homeworks.lesson19.async;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 public class RandomNumbers {
-    public static void main(String[] args) {
-        int N = 25;
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        int N = 5;
 
         List<Integer> randomNumber = new ArrayList<>();
         List<CompletableFuture<Integer>> completableFutureList = new ArrayList<>();
         Random random = new Random();
 
-        for (int i = 0; i < N; i++){
-            CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> random.nextInt());
+        for (int i = 0; i < N; i++) {
+            CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> random.nextInt(100));
             completableFutureList.add(completableFuture);
         }
 
-        //у каждого элемента completableFutureList вызвать метод get(), чтобы получить результат
+        /*for (CompletableFuture<Integer> future : completableFutureList){
+            System.out.print(future.get()+"\t");
+        }*/
+        for (CompletableFuture<Integer> future : completableFutureList) {
+            randomNumber.add(future.get());
+        }
+
+        System.out.println(randomNumber);
 
     }
 }
