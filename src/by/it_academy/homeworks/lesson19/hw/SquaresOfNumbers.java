11e7e91495ec0deccc7145dbf89eee94d6a0dc40
Stepa0425 <karapetyanstepa@gmail.com>
2024-03-26 11:48:13 +0300

    change counting  squares

--- src/by/it_academy/homeworks/lesson19/hw/SquaresOfNumbers.java
+++ src/by/it_academy/homeworks/lesson19/hw/SquaresOfNumbers.java
@@ -5,28 +5,24 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.regex.Matcher;
 
 public class SquaresOfNumbers {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         int N = 5;
 
-        List<Integer> randomNumber = new ArrayList<>();
         List<Double> squaresNumbers = new ArrayList<>();
-        List<CompletableFuture<Integer>> completableFutureList = new ArrayList<>();
+        List<CompletableFuture<Double>> completableFutureList = new ArrayList<>();
         Random random = new Random();
 
         for (int i = 0; i < N; i++) {
-            CompletableFuture<Integer> completableFuture = CompletableFuture.supplyAsync(() -> random.nextInt(100));
+            CompletableFuture<Double> completableFuture = CompletableFuture.supplyAsync(() -> Math.pow(random.nextInt(100), 2));
             completableFutureList.add(completableFuture);
         }
 
-        for (CompletableFuture<Integer> future : completableFutureList) {
-            randomNumber.add(future.get());
-            squaresNumbers.add(Math.pow(future.get(), 2));
+        for (CompletableFuture<Double> future : completableFutureList) {
+            squaresNumbers.add(future.get());
         }
 
-        System.out.println(randomNumber);
         System.out.println(squaresNumbers);
 
     }
