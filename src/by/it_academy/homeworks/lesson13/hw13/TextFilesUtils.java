7aec54150a7abb5c279772f88e3f29ba2f8e87bd
Stepa0425 <karapetyanstepa@gmail.com>
2024-03-27 12:02:12 +0300

    optimize code

--- src/by/it_academy/homeworks/lesson13/hw13/TextFilesUtils.java
+++ src/by/it_academy/homeworks/lesson13/hw13/TextFilesUtils.java
@@ -3,39 +3,32 @@ package by.it_academy.homeworks.lesson13.hw13;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.function.Function;
 
 public class TextFilesUtils {
 
     public static int getQuantityOfWords(String relativePath) {
         validateExtension(relativePath, ".txt");
-        int quantity = 0;
-        try (BufferedReader br = new BufferedReader(new FileReader(relativePath))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                quantity += countOfWords(line);
-            }
-        } catch (IOException e) {
-            System.out.println("Exception with message " + e.getMessage() + " occurred");
-        }
-        return quantity;
+        return processFile(relativePath, TextFilesUtils::countOfWords);
     }
 
     public static int getQuantityOfPunctuationMarks(String relativePath) {
         validateExtension(relativePath, ".txt");
+        return processFile(relativePath, TextFilesUtils::countOfPunctuationMarks);
+    }
+
+    private static int processFile(String relativePath, Function<String, Integer> countFunction){
         int quantity = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(relativePath))) {
             String line;
             while ((line = br.readLine()) != null) {
-                quantity += countOfPunctuationMarks(line);
+                quantity += countFunction.apply(line);
             }
         } catch (IOException e) {
             System.out.println("Exception with message " + e.getMessage() + " occurred");
         }
         return quantity;
     }
-
     private static void validateExtension(String path, String extension) {
         if (!path.endsWith(extension)) {
             throw new RuntimeException("The file doesn't have the " + extension + " extension.");
