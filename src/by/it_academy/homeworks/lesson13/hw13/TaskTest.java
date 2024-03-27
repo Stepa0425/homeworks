8c186122278f7035648e8e020130cee3dd3de26a
Stepa0425 <karapetyanstepa@gmail.com>
2024-03-27 12:06:56 +0300

    solve problem with package lesson 13

--- src/by/it_academy/homeworks/lesson13/hw13/TaskTest.java
+++ src/by/it_academy/homeworks/lesson13/hw13/TaskTest.java
@@ -2,9 +2,9 @@ package by.it_academy.homeworks.lesson13.hw13;
 //Создать файл с текстом, прочитать, подсчитать в тексте количество знаков препинания и слов.
 public class TaskTest {
     public static void main(String[] args) {
-        final String PATH_TO_FILE_1 = "resources/homework13.txt";
-        final String PATH_TO_FILE_3 = "resources/ССП 1 лаба.docx";
-        final String PATH_TO_FILE_2 = "resources/EmptyFile";
+        final String PATH_TO_FILE_1 = "resources/lesson13/homework13.txt";
+        final String PATH_TO_FILE_3 = "resources/lesson13/ССП 1 лаба.docx";
+        final String PATH_TO_FILE_2 = "resources/lesson13/EmptyFile";
 
 
         System.out.println("Quantity of punctuation marks in files is " + TextFilesUtils.getQuantityOfPunctuationMarks(PATH_TO_FILE_1));
